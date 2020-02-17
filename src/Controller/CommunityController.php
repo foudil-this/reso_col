c1e32fdce95a3ff65cbfa3fe109256d31877e064
pierrick <pierrickviaud19@gmail.com>
2020-02-17 17:27:36 +0100

    community cree

--- src/Controller/CommunityController.php
+++ src/Controller/CommunityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Community;
 use App\Form\CommunityType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -64,18 +65,25 @@ class CommunityController extends AbstractController
                         $community->setImage($imageFileName);
                     }
                     $owner = $this->getUser();
-                    $community->setOwner($owner);
+                    $community
+                        ->setOwner($owner)
+                        // ajout dans dans community de l'owner
+                        ->addUser($owner)
+                    ;
 
-                    // insertion dans la BDD
+                    // insertion dans la BDD, mise à jour des 2 tables
+                    // table community et table community user
                     $manager->persist($community);
                     $manager->flush();
+
+
 
 
                     $this->addFlash('success', 'Votre association est créee');
 
                     // dump($user);
                     // retour de l'objet userController vers index
-                    return $this->redirectToRoute('app_index_index');
+                    return $this->redirectToRoute('app_community_addusercommunity', ['id' => $community->getId()]);
 
 
                 } else {
@@ -87,5 +95,49 @@ class CommunityController extends AbstractController
         return $this->render('community/edit.html.twig',
             [ 'form' => $form->createView()]
         );
+    }
+
+
+    /**
+     * @Route("/adduser/{id}")
+     *
+     *
+     */
+    public function addUserCommunity(Community $community, Request $request, UserRepository $userRepository, EntityManagerInterface $manager, $id)
+    {
+        $users = $community->getUsers();
+
+
+        $owner = $community->getOwner();
+        dump($users[0]);
+        // test si il y a bien un email saisi dans la requete
+        if($request->request->has('email_user_add')){
+            // recherche dans la table user du user ayant l'email saisi
+            $userAdded= $userRepository->findOneBy(
+                ['email' => $request->request->get('email_user_add')]
+            );
+
+            if ($community->getUsers()->contains($userAdded)) {
+                $this->addFlash('error', 'le membre est dejà dans cette association');
+            }
+
+            dump($userAdded);
+            dump($community);
+
+
+            // ajout dans community du user saisi dans le formulaire (son email)
+            $community->addUser($userAdded);
+
+
+
+            // connexion à la BDD
+            $manager->persist($community);
+            $manager->flush();
+        }
+
+
+        dump($request);
+        return $this->render('community/addUserCommunity.html.twig', ['community' => $community,
+            'users' => $users]);
     }
 }
