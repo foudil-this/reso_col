c38fe3bee71969e96d54e4815e1c193a1f13ab1c
pierrick <pierrickviaud19@gmail.com>
2020-02-19 23:01:51 +0100

    mercredi 23h

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -28,7 +28,22 @@ class UserController extends AbstractController
                              UserPasswordEncoderInterface $passwordEncoder,
                              EntityManagerInterface $manager)
     {
-        $user = new User();
+
+        // creation variable pour le nom de l'ancienne image
+        // pour le user à modifier
+        $oldAvatar=null;
+
+        if(is_null($this->getUser())){
+            // nouveau user
+            $user = new User();
+        }else{
+            // user à modifier (le connecté)
+            $user =$this->getUser();
+            $oldAvatar = $user->getAvatar();
+            if(!is_null($oldAvatar)){
+                $user->setAvatar(new File($this->getParameter('upload_dir') . $oldAvatar));
+            }
+        }
 
         $form = $this->createForm(RegistrationType::class, $user);
 
@@ -62,29 +77,45 @@ class UserController extends AbstractController
                     // on sette l'image' de l'article avec le nom du fichier
                     // pour enregistrement
                     $user->setAvatar($avatarfilename);
+
+                    // en modification on supprime l'ancienne image
+                    // si il y en a une
+                    // avant d'ajouter la nouvelle
+                    if (!is_null($oldAvatar)) {
+                        unlink($this->getParameter('upload_dir') . $oldAvatar);
+                    }
+
+                }else {
+                    // en modification, sans upload, on sette l'image
+                    // avec le nom de l'ancienne image
+                    if (!is_null($oldAvatar)) {
+                        $user->setavatar($oldAvatar);
+                    }
+
                 }
+
 
                     // insertion dans la BDD
                     $manager->persist($user);
                     $manager->flush();
 
 
-                    $this->addFlash('success', 'Votre compte est crée');
+                    $this->addFlash('success', 'Votre compte est opérationnel');
 
                     // dump($user);
                     // retour de l'objet userController vers index
                     return $this->redirectToRoute('app_index_index');
-
-
-            } else {
-                $this->addFlash('error', 'le formulaire contient des erreurs');
             }
 
+            else {
+                $this->addFlash('error', 'le formulaire contient des erreurs');
+            }
         }
+
         // retour de l'objet userController vers inscription
         return $this->render(
             'user/register.html.twig',
-            ['form' => $form->createView()]
+            ['form' => $form->createView(), 'oldAvatar' => $oldAvatar]
         );
     }
 
@@ -119,88 +150,5 @@ class UserController extends AbstractController
 
     }
 
-
-    /**
-     * @Route("/profil")
-     *
-     */
-    public function changeRegistration(Request $request, EntityManagerInterface $manager,
-                                       UserPasswordEncoderInterface $passwordEncoder)
-    {
-
-        // creation variable pour le nom de l'ancienne image
-        $oldAvatar=null;
-
-        // creation de l'utilisateur à modifier (le connecté)
-        $userToChange=$this->getUser();
-        dump($userToChange);
-        $oldAvatar = $userToChange->getAvatar();
-        if(!is_null($oldAvatar)){
-            $userToChange->setAvatar(new File($this->getParameter('upload_dir') . $oldAvatar));
-        }
-
-        $form = $this->createForm(RegistrationType::class, $userToChange);
-
-        $form->handleRequest($request);
-        if($form->isSubmitted()) {
-            if ($form->isValid()) {
-
-                // cryptage du mot de passe
-                $encodedPassword = $passwordEncoder->encodePassword(
-                    $userToChange,
-                    $userToChange->getPlainpassword()
-                );
-
-                $userToChange->setPassword($encodedPassword);
-
-                /** @var    UploadedFile|null $avatar */
-                $avatar = $userToChange->getAvatar();
-
-
-                // test si une image est saisie dans le formulaire
-                if (!is_null($avatar)) {
-                    // nom pour la BDD
-                    $filename = uniqid() . '.' . $avatar->guessExtension();
-
-                    // deplacer le fichier vers le repertoire de stockage
-                    $avatar->move(
-                    // repertoire de destination fait dans config/services.yaml
-                        $this->getParameter('upload_dir'),
-                        // nom du fichier
-                        $filename
-                    );
-
-                    // on sette l'image' de l'article avec le nom du fichier
-                    // pour enregistrement
-                    $userToChange->setAvatar($filename);
-
-                    // en modification on supprime l'ancienne image
-                    // si il y en a une
-                    // et avant d'ajouter la nouvelle
-                    if (!is_null($oldAvatar)) {
-                        unlink($this->getParameter('upload_dir') . $oldAvatar);
-                    }
-                } else {
-
-                    // en modification, sans upload, on sette l'image
-                    // avec le nom de l'ancienne image
-                    $userToChange->setavatar($oldAvatar);
-                }
-                // insertion dans la BDD
-                $manager->persist($userToChange);
-                $manager->flush();
-                $this->addFlash('success', 'Votre compte est modifié');
-
-                return $this->redirectToRoute('app_index_index');
-            } else {
-                $this->addFlash('error', 'le formulaire contient des erreurs');
-            }
-
-        }
-        return $this->render('user/register.html.twig',
-            ['form' => $form->createView(), 'oldAvatar' => $oldAvatar]
-            );
-    }
-
-
+    
 }
